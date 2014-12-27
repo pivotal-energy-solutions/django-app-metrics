cc2bf454fa34122f0ee00ead04a42d6253025ffd
Steven Klass <sklass@pointcircle.com>
2014-12-27 15:13:46 -0700

    Add in assign_user_to_latest_history as a generic method.

--- app_metrics/managers.py
+++ app_metrics/managers.py
@@ -4,7 +4,6 @@
 from __future__ import unicode_literals
 
 import logging
-from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Q
 from apps.relationship.models import Relationship
