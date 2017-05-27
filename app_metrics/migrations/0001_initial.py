10c34d02c9e239eeef44f0c07fa3567848fa88d0
Tim Valenta <tonightslastsong@gmail.com>
2017-05-26 17:04:07 -0800

    I lied, we depend on Axis, just not 0014

--- app_metrics/migrations/0001_initial.py
+++ app_metrics/migrations/0001_initial.py
@@ -15,6 +15,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('company', '0001_initial'),
     ]
 
     operations = [
