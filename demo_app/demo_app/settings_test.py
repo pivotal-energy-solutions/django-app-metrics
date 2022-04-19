ed065dc9b6c48e77a362f313ba76ff6982dbbcfb
Steven K <steven@pointcircle.com>
2022-04-19 16:23:14 -0700

    Unify the database and simply for tests.

--- demo_app/demo_app/settings_test.py
+++ demo_app/demo_app/settings_test.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
 
 import warnings
 
-from .settings import *
-
-
-class DisableMigrations(object):
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-MIGRATION_MODULES = DisableMigrations()
+from .settings import *  # noqa: F403,F401
 
 # Handle system warning as log messages
 warnings.simplefilter("once")
 
+for handler in LOGGING.get("handlers", []):
+    LOGGING["handlers"][handler]["level"] = "CRITICAL"
+for logger in LOGGING.get("loggers", []):
+    LOGGING["loggers"][logger]["level"] = "CRITICAL"
 
 mysql_db = DATABASES["default"]
-DEFAULT_DB = {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
+DEFAULT_DB = {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": ":memory:",
+    "TEST": {"MIGRATE": False},
+}
 if os.environ.get("DB_TYPE") == "mysql":
     print("Using MySQL Backend!")
     DEFAULT_DB = mysql_db
@@ -29,6 +25,7 @@ if os.environ.get("DB_TYPE") == "mysql":
 DATABASES = {
     "default": DEFAULT_DB,
 }
+
 
 CELERY_TASK_ALWAYS_EAGER = CELERY_TASK_EAGER_PROPAGATES = True
 CELERYD_HIJACK_ROOT_LOGGER = True
