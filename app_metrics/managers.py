1d3475fb7ba931f208d7339170fe7a126976a959
Steven <rh0dium@users.noreply.github.com>
2023-01-01 15:05:06 -0700

    Copyright 2023 (#3)
    * Copyright 2023
    * Black

--- app_metrics/managers.py
+++ app_metrics/managers.py
@@ -10,7 +10,7 @@ from django.db.models import Q
 
 __author__ = "Steven Klass"
 __date__ = "4/3/13 5:26 AM"
-__copyright__ = "Copyright 2011-2020 Pivotal Energy Solutions. All rights reserved."
+__copyright__ = "Copyright 2011-2023 Pivotal Energy Solutions. All rights reserved."
 __credits__ = [
     "Steven Klass",
 ]
