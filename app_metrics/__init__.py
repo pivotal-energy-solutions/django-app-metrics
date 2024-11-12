0e9072699e716560685f669c84a4ba75e1ad40f6
Steven K <steven@pointcircle.com>
2024-11-12 21:12:59 +0000

    Version Bump [skip ci]

--- app_metrics/__init__.py
+++ app_metrics/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "app_metrics"
 __author__ = "Pivotal Energy Solutions"
-__version_info__ = (4, 0, 12)
-__version__ = "4.0.12"
+__version_info__ = (5, 0, 0)
+__version__ = "5.0.0"
 __date__ = "2014/07/22 4:47:00 PM"
 __credits__ = [
     "Frank Wiles",
